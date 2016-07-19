597994a623d56892da461222a009c874533f2adf
John S. Ryan <jtigger@infosysengr.com>
2016-07-18 18:11:32 -0700

    Add skeleton of journey test.

---
+++ app/src/main/java/io/pivotal/pairwithme/viewoffers/ViewOffersActivity.java
@@ -0,0 +1,15 @@
+package io.pivotal.pairwithme.viewoffers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.pivotal.pairwithme.R;
+
+public class ViewOffersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_offers);
+    }
+}
