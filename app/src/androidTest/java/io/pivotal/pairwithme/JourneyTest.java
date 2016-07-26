da28c74207c4e3b71e6ffc4a503cc62cb4878577
John S. Ryan <jtigger@infosysengr.com>
2016-07-25 17:22:34 -0700

    [#126925661] Add RecyclerView for pairing sessions list.
    - CI Improvements:
      - Install Google Repository on CircleCI.
      - Disable pre-dexing in CI.
      - Unlock device before running tests.

--- app/src/androidTest/java/io/pivotal/pairwithme/JourneyTest.java
+++ app/src/androidTest/java/io/pivotal/pairwithme/JourneyTest.java
@@ -3,8 +3,11 @@ package io.pivotal.pairwithme;
 import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.app.AppCompatActivity;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.WindowManager;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +17,7 @@ import io.pivotal.pairwithme.viewschedule.ViewScheduleActivity;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withChild;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
@@ -24,11 +28,28 @@ public class JourneyTest {
             new ActivityTestRule<>(ViewScheduleActivity.class, true, false);
 
 
+    private void unlockDevice() {
+        final AppCompatActivity activity = initialActivity.getActivity();
+        Runnable wakeUpDevice = new Runnable() {
+            public void run() {
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        };
+        activity.runOnUiThread(wakeUpDevice);
+    }
+
     @Test
     public void KathyAndKevinFindATimeToPair() {
         initialActivity.launchActivity(new Intent());
+        unlockDevice();
 
-        onView(withId(R.id.view_schedule_screen)).check(matches(isDisplayed()));
-        onView(withId(R.id.pairing_sessions_list)).check(matches(isDisplayed()));
+        onView(withId(R.id.view_schedule_screen))
+                .check(matches(isDisplayed()));
+
+        onView(withId(R.id.pairing_sessions_list))
+                .check(matches(isDisplayed()))
+                .check(matches(withChild(isDisplayed())));
     }
 }
--- app/src/main/java/io/pivotal/pairwithme/viewschedule/ScheduleFragment.java
+++ app/src/main/java/io/pivotal/pairwithme/viewschedule/ScheduleFragment.java
@@ -3,10 +3,13 @@ package io.pivotal.pairwithme.viewschedule;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import io.pivotal.pairwithme.R;
 
@@ -18,9 +21,55 @@ public class ScheduleFragment extends Fragment {
     public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView() called with: " + "inflater = [" + inflater + "], container = [" + container + "], savedInstanceState = [" + savedInstanceState + "]");
 
-        View root = inflater.inflate(R.layout.schedule, container, false);
+        View rootView = inflater.inflate(R.layout.schedule, container, false);
+        RecyclerView sessionListView = (RecyclerView) rootView.findViewById(R.id.pairing_sessions_list);
 
-        Log.d(TAG, "onCreateView() returned: " + root);
-        return root;
+        RecyclerView.LayoutManager sessionListLayoutManager = new LinearLayoutManager(getActivity());
+        sessionListView.setLayoutManager(sessionListLayoutManager);
+
+        RecyclerView.Adapter sessionListAdapter = new SessionListAdapter();
+        sessionListView.setAdapter(sessionListAdapter);
+
+        Log.d(TAG, "onCreateView() returned: " + rootView);
+        return rootView;
+    }
+
+    public static class SessionListAdapter extends RecyclerView.Adapter<SessionListAdapter.ViewHolder> {
+        private String[] scheduleDates = {
+                "Python: 3pm-6pm on July 26, 2016",
+                "Ruby: 12pm-1pm on July 27, 2016",
+                "Python: 9am-12pm on July 28, 2016",
+                "Java: 12pm-3pm on July 29, 2016"
+        };
+
+        @Override
+        public int getItemCount() {
+            return scheduleDates.length;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.session, parent, false);
+            return new ViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, final int position) {
+            holder.setDetail(scheduleDates[position]);
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            private TextView sessionDetailTextView;
+
+            public ViewHolder(final View itemView) {
+                super(itemView);
+                sessionDetailTextView = (TextView) itemView.findViewById(R.id.session_detail);
+            }
+
+            public void setDetail(String detail) {
+                sessionDetailTextView.setText(detail);
+            }
+        }
     }
 }
